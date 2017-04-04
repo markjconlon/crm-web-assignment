2923915be58cde322aea6dc15eb5d930dac2e9bf
Mark <markjconlon@gmail.com>
2017-04-04 13:40:55 -0400

    finished the assignment will continue to expand on it

--- crm.rb
+++ crm.rb
@@ -4,9 +4,9 @@
 require_relative 'contact'
 require 'sinatra'
 
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Mark's CRM"
@@ -15,4 +15,13 @@ end
 
 get '/contacts' do
   erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
