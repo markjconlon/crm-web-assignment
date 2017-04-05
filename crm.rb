b571d5c52e88a27967d807bbdad5bbb4f15e5e2e
Mark <markjconlon@gmail.com>
2017-04-05 16:21:14 -0400

    delete contact button works

--- crm.rb
+++ crm.rb
@@ -59,6 +59,15 @@ put '/contacts/:id' do
   end
 end
 
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 
 # #this only works because of sinatra, pure HTTP only knows get and post
