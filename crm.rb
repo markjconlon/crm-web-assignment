2bbd1f77202f1783ada7e1eeff24d5f20a1e89ec
Mark <markjconlon@gmail.com>
2017-04-05 16:14:51 -0400

    filled form for editing contacts

--- crm.rb
+++ crm.rb
@@ -45,7 +45,19 @@ get '/contacts/:id/edit' do
   end
 end
 
+put '/contacts/:id' do
+  @contact= Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
 
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 
 
