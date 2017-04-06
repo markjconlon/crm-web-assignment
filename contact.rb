d46ab92e1193faec22909fa2d53bcfef8dbda786
Mark <markjconlon@gmail.com>
2017-04-06 13:06:38 -0400

    adding a contact to SQL complete

--- contact.rb
+++ contact.rb
@@ -5,11 +5,11 @@ require 'mini_record'
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
 
 class Contact < ActiveRecord::Base
-
-  field :first_name, as :string #string field is limited to 256 characters
-  field :last_name, as :string
-  field :email, as :string
-  field :note,  as :text
+  #string field is limited to 256 characters
+  field :first_name, as: :string
+  field :last_name, as: :string
+  field :email, as: :string
+  field :note,  as: :text
 
   # attr_accessor :first_name, :last_name, :email, :note
   # attr_reader :id
@@ -21,6 +21,3 @@ Contact.auto_upgrade!
 #this is required because MiniRecord will open connections but wont close
 #SQLite only allows 5 connections so every 6th time you reconnect you
 #would otherwise get a timeout ERROR
-after do
-  ActiveRecord::Base.connection.close
-end
